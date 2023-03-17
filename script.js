e6be8c7fb4553f976799815b42a7e7dae2e99867
Re-Santos <renata.prog@gmail.com>
2023-03-17 15:37:38 -0300

    🐛 fix: button não funciona

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+
 function createCloseButton(li){
     let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
+    let txt = document.createTextNode("🗑️");
 
     span.className = "close";
     span.appendChild(txt);
@@ -9,13 +10,13 @@ function createCloseButton(li){
     span.onclick = () => span.parentElement.style.display = "none";
 }
 document.querySelectorAll("li").forEach(createCloseButton);
-document.querySelector("ul").addEventListener("click", (e)=>{
+document.querySelector("ul").addEventListener("click", (e) => {
     if(e.target.tagName === "LI")
     e.target.classList.toggle("checked");
 });
 
-function add(){
-    let li = document.createAttribute.Element("Li");
+function add() {
+    let li = document.createElement("LI");
     let input_value = document.form_main.task.value;
     let input_text = document.createTextNode(input_value);
 
